de3f2a96606f73d9e6a1856913630bcf8515a3e6
Rakotomavo Manoa Harivony Ellias <114306980+HarivonyEllias@users.noreply.github.com>
2024-01-27 15:17:44 +0300

    Add files via upload

--- src/routes.js
+++ src/routes.js
@@ -22,56 +22,70 @@ import Register from "views/examples/Register.js";
 import Login from "views/examples/Login.js";
 import Tables from "views/examples/Tables.js";
 import Icons from "views/examples/Icons.js";
+import Categorie from "views/crud/Categorie.js";
+import Marque from "views/crud/Marque";
+import Modele from "views/crud/Modele";
+import Type from "views/crud/Type";
+import Devise from "views/crud/Devise";
+import Commission from "views/crud/Commission";
+import Annonce from "views/annnonce/Annonce";
 
 var routes = [
   {
     path: "/index",
-    name: "Dashboard",
-    icon: "ni ni-tv-2 text-primary",
+    name: "Statistiques",
+    icon: "ni ni-chart-bar-32 text-primary",
     component: <Index />,
     layout: "/admin",
   },
   {
-    path: "/icons",
-    name: "Icons",
-    icon: "ni ni-planet text-blue",
-    component: <Icons />,
+    path: "/annonces",
+    name: "Annonce",
+    icon: "ni ni-single-copy-04 text-green",
+    component: <Annonce />,
     layout: "/admin",
   },
   {
-    path: "/maps",
-    name: "Maps",
-    icon: "ni ni-pin-3 text-orange",
-    component: <Maps />,
+    path: "/categorie",
+    name: "Categorie",
+    icon: "ni ni-books text-purple",
+    component: <Categorie />,
     layout: "/admin",
   },
   {
-    path: "/user-profile",
-    name: "User Profile",
-    icon: "ni ni-single-02 text-yellow",
-    component: <Profile />,
+    path: "/marque",
+    name: "Marque",
+    icon: "ni ni-planet text-orange",
+    component: <Marque />,
     layout: "/admin",
   },
   {
-    path: "/tables",
-    name: "Tables",
-    icon: "ni ni-bullet-list-67 text-red",
-    component: <Tables />,
+    path: "/modele",
+    name: "Modele",
+    icon: "ni ni-bullet-list-67 text-yellow",
+    component: <Modele />,
     layout: "/admin",
   },
   {
-    path: "/login",
-    name: "Login",
-    icon: "ni ni-key-25 text-info",
-    component: <Login />,
-    layout: "/auth",
+    path: "/type",
+    name: "Type",
+    icon: "ni ni-map-big text-red",
+    component: <Type />,
+    layout: "/admin",
   },
   {
-    path: "/register",
-    name: "Register",
-    icon: "ni ni-circle-08 text-pink",
-    component: <Register />,
-    layout: "/auth",
+    path: "/devise",
+    name: "Devise",
+    icon: "ni ni-credit-card text-info",
+    component: <Devise />,
+    layout: "/admin",
+  },
+  {
+    path: "/commission",
+    name: "Commission",
+    icon: "ni ni-check-bold text-pink",
+    component: <Commission />,
+    layout: "/admin",
   },
 ];
 export default routes;
