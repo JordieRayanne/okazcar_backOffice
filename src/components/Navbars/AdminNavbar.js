a60036a3f774f26acd2798875ef83525cfa996fd
Rakotomavo Manoa Harivony Ellias <114306980+HarivonyEllias@users.noreply.github.com>
2024-01-28 21:38:50 +0300

    Add files via upload

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -38,7 +38,7 @@ const AdminNavbar = (props) => {
   return (
     <>
       <Navbar className="navbar-top navbar-dark" expand="md" id="navbar-main">
-        <Container fluid>
+        {/* <Container fluid>
           <Link
             className="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block"
             to="/"
@@ -102,7 +102,7 @@ const AdminNavbar = (props) => {
               </DropdownMenu>
             </UncontrolledDropdown>
           </Nav>
-        </Container>
+        </Container> */}
       </Navbar>
     </>
   );
