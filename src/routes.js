a089b6ad554239eefb3d9f620382941016e31598
IrinaKaren <121242769+IrinaKaren@users.noreply.github.com>
2024-01-28 23:08:34 +0300

    Add files via upload

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,3 @@
-/*!
-
-=========================================================
-* Argon Dashboard React - v1.2.4
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
-* Copyright 2024 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import Index from "views/Index.js";
 import Profile from "views/examples/Profile.js";
 import Maps from "views/examples/Maps.js";
@@ -22,6 +5,7 @@ import Register from "views/examples/Register.js";
 import Login from "views/examples/Login.js";
 import Tables from "views/examples/Tables.js";
 import Icons from "views/examples/Icons.js";
+import Voiture from "views/crud/Voiture.js";
 import Categorie from "views/crud/Categorie.js";
 import Marque from "views/crud/Marque";
 import Modele from "views/crud/Modele";
@@ -43,6 +27,13 @@ var routes = [
     name: "Annonce",
     icon: "ni ni-single-copy-04 text-green",
     component: <Annonce />,
+    layout: "/admin",
+  },
+  {
+    path: "/voitures",
+    name: "Voiture",
+    icon: "ni ni-bus-front-12 text-brown",
+    component: <Voiture />,
     layout: "/admin",
   },
   {
