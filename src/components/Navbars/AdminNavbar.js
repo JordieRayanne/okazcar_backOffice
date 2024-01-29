d181aeeab5b502579cc887994ded06374802aa4b
Rakotomavo Manoa Harivony Ellias <114306980+HarivonyEllias@users.noreply.github.com>
2024-01-29 22:20:08 +0300

    Update AdminNavbar.js

--- src/components/Navbars/AdminNavbar.js
+++ src/components/Navbars/AdminNavbar.js
@@ -1,9 +1,82 @@
+/*!
 
+=========================================================
+* Argon Dashboard React - v1.2.4
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2024 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import {Link, useNavigate} from "react-router-dom";
+// reactstrap components
+import {
+  DropdownMenu,
+  DropdownItem,
+  UncontrolledDropdown,
+  DropdownToggle,
+  Navbar,
+  Nav,
+  Container,
+  Media,
+} from "reactstrap";
+import {useAuthUser} from "react-auth-kit";
 
 const AdminNavbar = (props) => {
+  const authUser = useAuthUser()
+  const navigate= useNavigate()
+  const signOut = () => {
+    localStorage.clear()
+    navigate("/auth/login")
+  }
   return (
     <>
-      {props}
+      <Navbar className="navbar-top navbar-dark" expand="md" id="navbar-main">
+        <Container fluid>
+          <Link
+            className="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block"
+            to="/"
+          >
+            {props.brandText}
+          </Link>
+
+          <Nav className="align-items-center d-none d-md-flex" navbar>
+            <UncontrolledDropdown nav>
+              <DropdownToggle className="pr-0" nav>
+                <Media className="align-items-center">
+                  <span className="avatar avatar-sm rounded-circle">
+                    <img
+                      alt="..."
+                      src={"data:image/jpeg;base64,"+authUser().image}
+                    />
+                  </span>
+                  <Media className="ml-2 d-none d-lg-block">
+                    <span className="mb-0 text-sm font-weight-bold">
+                      {authUser().username}
+                    </span>
+                  </Media>
+                </Media>
+              </DropdownToggle>
+              <DropdownMenu className="dropdown-menu-arrow" right>
+                <DropdownItem className="noti-title" header tag="div">
+                  <h6 className="text-overflow m-0">Welcome!</h6>
+                </DropdownItem>
+                <DropdownItem tag="div" onClick={signOut}>
+                  <i className="ni ni-user-run" />
+                  <span>Logout</span>
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>
+          </Nav>
+        </Container>
+      </Navbar>
     </>
   );
 };
