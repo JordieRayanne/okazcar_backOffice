8c8c3f3e26ae46fb48d0596dce6fbe14285d2c0b
Rakotomavo Manoa Harivony Ellias <114306980+HarivonyEllias@users.noreply.github.com>
2024-01-29 14:34:24 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,26 +13,86 @@ import Annonce from "views/annnonce/Annonce";
 import Index from "views/Index";
 import routes from "routes";
 import Icons from "views/examples/Icons";
+import Categorie from "views/crud/Categorie";
+import Modele from "views/crud/Modele";
+import Marque from "views/crud/Marque";
+import Type from "views/crud/Type";
+import Devise from "views/crud/Devise";
+import Commission from "views/crud/Commission";
 
 function App(){
     return(
         <>
            <Routes>
-           <Route path="/admin/annonces" element={
-                <>
-                    <Sidebar 
-                    routes={routes}
-                    />
-                    <Icons />
-                </>
-            } 
-            />
             <Route path="/admin/statistiques" element={
                 <>
                     <Sidebar 
                     routes={routes}
                     />
                     <Index />
+                </>
+            } 
+            />
+           <Route path="/admin/annonces" element={
+                <>
+                    <Sidebar 
+                    routes={routes}
+                    />
+                    <Annonce />
+                </>
+            } 
+            />
+            <Route path="/admin/categorie" element={
+                <>
+                    <Sidebar 
+                    routes={routes}
+                    />
+                    <Categorie />
+                </>
+            } 
+            />
+            <Route path="/admin/modele" element={
+                <>
+                    <Sidebar 
+                    routes={routes}
+                    />
+                    <Modele />
+                </>
+            } 
+            />
+            <Route path="/admin/marque" element={
+                <>
+                    <Sidebar 
+                    routes={routes}
+                    />
+                    <Marque />
+                </>
+            } 
+            />
+            <Route path="/admin/type" element={
+                <>
+                    <Sidebar 
+                    routes={routes}
+                    />
+                    <Type />
+                </>
+            } 
+            />
+            <Route path="/admin/devise" element={
+                <>
+                    <Sidebar 
+                    routes={routes}
+                    />
+                    <Devise />
+                </>
+            } 
+            />
+            <Route path="/admin/commission" element={
+                <>
+                    <Sidebar 
+                    routes={routes}
+                    />
+                    <Commission />
                 </>
             } 
             />
