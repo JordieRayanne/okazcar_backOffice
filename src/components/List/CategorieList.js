1b85d05730db50bcbfd623aa6baa023c0f151dea
Rakotomavo Manoa Harivony Ellias <114306980+HarivonyEllias@users.noreply.github.com>
2024-01-29 15:59:29 +0300

    Update CategorieList.js

--- src/components/List/CategorieList.js
+++ src/components/List/CategorieList.js
@@ -3,29 +3,26 @@ import axios from "axios";
 import {
     Card,
     CardHeader,
-    CardFooter,
     DropdownMenu,
     DropdownItem,
     UncontrolledDropdown,
     DropdownToggle,
-    Pagination,
-    PaginationItem,
-    PaginationLink,
     Table,
     Input,
     Button,
   } from "reactstrap";
+import {useAuthHeader} from "react-auth-kit";
 function CategorieList(){
   const[Categories,setCategories]=useState([]);
   const[editingId, setEditingId]=useState(null);
   const[newNom,setNewNom]=useState(null);
+  const token = useAuthHeader()
 
   const listCategories=()=>{
-    const token = 'eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiUk9MRV9BRE1JTiIsInN1YiI6Im1haGZpdGFoaWFuYUBnbWFpbC5jb20iLCJpYXQiOjE3MDY0NjE4NDEsImV4cCI6MTcwNjQ2OTA0MX0.-Jn5DPKV6ZiAR4kEXsjyq5YCTqZR5WoQMhnuxul4ihs'; // Replace with your actual token
-    
+
     axios.get('https://okazcar.up.railway.app/categories', {
       headers: {
-        Authorization: `Bearer ${token}`
+        Authorization: token()
       }
     })
       .then(Response=>{
@@ -43,11 +40,11 @@ function CategorieList(){
   };
 
   const handleUpdate = () => {
-    const token = 'eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiUk9MRV9BRE1JTiIsInN1YiI6Im1haGZpdGFoaWFuYUBnbWFpbC5jb20iLCJpYXQiOjE3MDY0NjE4NDEsImV4cCI6MTcwNjQ2OTA0MX0.-Jn5DPKV6ZiAR4kEXsjyq5YCTqZR5WoQMhnuxul4ihs'; // Replace with your actual token
-
-    axios.put(`https://okazcar.up.railway.app/categories/${editingId}`, { nom: newNom }, {
+      const formData = new FormData()
+      formData.append("nom", newNom)
+    axios.put(`https://okazcar.up.railway.app/categories/${editingId}`, formData, {
       headers: {
-        Authorization: `Bearer ${token}`
+        Authorization: token()
       }
     })
       .then(response => {
@@ -68,11 +65,9 @@ function CategorieList(){
 
   const handleDelete = (id) => {
     console.log('ID à supprimer:', id);
-    const token = 'eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiUk9MRV9BRE1JTiIsInN1YiI6Im1haGZpdGFoaWFuYUBnbWFpbC5jb20iLCJpYXQiOjE3MDY0NjE4NDEsImV4cCI6MTcwNjQ2OTA0MX0.-Jn5DPKV6ZiAR4kEXsjyq5YCTqZR5WoQMhnuxul4ihs'; // Replace with your actual token
-    
     axios.delete(`https://okazcar.up.railway.app/categories/${id}`, {
       headers: {
-        Authorization: `Bearer ${token}`
+        Authorization: token()
       }
     })
       .then(response => {
@@ -98,7 +93,7 @@ function CategorieList(){
   return (
     <Card className="shadow">
       <CardHeader className="border-0">
-        <h3 className="mb-0">Liste Categorie</h3>
+        <h3 className="mb-0">Liste Catégories</h3>
       </CardHeader>
       <Table className="align-items-center table-flush" responsive>
         <thead className="thead-light">
@@ -167,58 +162,6 @@ function CategorieList(){
           ))}
         </tbody>
       </Table>
-      <CardFooter className="py-4">
-        <nav aria-label="...">
-          <Pagination
-            className="pagination justify-content-end mb-0"
-            listClassName="justify-content-end mb-0"
-          >
-            <PaginationItem className="disabled">
-              <PaginationLink
-                href="#pablo"
-                onClick={(e) => e.preventDefault()}
-                tabIndex="-1"
-              >
-                <i className="fas fa-angle-left" />
-                <span className="sr-only">Previous</span>
-              </PaginationLink>
-            </PaginationItem>
-            <PaginationItem className="active">
-              <PaginationLink
-                href="#pablo"
-                onClick={(e) => e.preventDefault()}
-              >
-                1
-              </PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationLink
-                href="#pablo"
-                onClick={(e) => e.preventDefault()}
-              >
-                2 <span className="sr-only">(current)</span>
-              </PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationLink
-                href="#pablo"
-                onClick={(e) => e.preventDefault()}
-              >
-                3
-              </PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationLink
-                href="#pablo"
-                onClick={(e) => e.preventDefault()}
-              >
-                <i className="fas fa-angle-right" />
-                <span className="sr-only">Next</span>
-              </PaginationLink>
-            </PaginationItem>
-          </Pagination>
-        </nav>
-      </CardFooter>
     </Card>
   );
 }
