5798905815c8e145308ef443a3bce57a2cbf02fb
Rakotomavo Manoa Harivony Ellias <114306980+HarivonyEllias@users.noreply.github.com>
2024-01-29 23:56:33 +0300

    Update AnnonceList.js

--- src/components/List/AnnonceList.js
+++ src/components/List/AnnonceList.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Card, CardBody, CardTitle, Container, Row, Col, UncontrolledTooltip, Button } from 'reactstrap';
-import { useAuthHeader } from 'react-auth-kit';
+import { RingLoader } from 'react-spinners'; // Import the spinner component
+import token from 'token';
+
 const AnnonceList = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [isListValidated, setisListValidated] = useState(false);
-  const token = useAuthHeader();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -30,44 +31,59 @@ const AnnonceList = () => {
   
     fetchData();
   }, []);
-
+  
   const handleValidate = async (annonceId) => {
     try {
       const response = await fetch(`https://okazcar.up.railway.app/voitureUtilisateurs/${annonceId}/to-10`, {
-        method: 'POST',
+        method: 'POST', 
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': token(),
+          Authorization: `Bearer ${token}`,
         },
       });
-
+  
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
-
+  
       console.log('Validation successful!');
-      await handleAnnonceClick();
+      handleAnnonceClick();
     } catch (error) {
       console.error('Error during validation:', error);
-      await handleAnnonceClick();
+      handleAnnonceClick();
+
     }
   };
 
   const handleReject = async (annonceId) => {
     try {
+      // const response = await fetch(`http://localhost:8080/voitureUtilisateurs/${annonceId}/to-10`, {
+      //   method: 'POST', 
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //     Authorization: `Bearer ${token}`,
+      //   },
+      // });
+  
+      // if (!response.ok) {
+      //   throw new Error(`HTTP error! Status: ${response.status}`);
+      // }
+  
       alert('Rejected');
     } catch (error) {
       console.error('Error during validation:', error);
     }
   };
-
+  
   const handleAnnonceClick = async () => {
     try {
       setLoading(true);
       await new Promise(resolve => setTimeout(resolve, 1000));
+
+  
       const response = await fetch('https://okazcar.up.railway.app/voitureUtilisateurs_not_validated', {
         headers: {
-          'Authorization': token()
+          Authorization: `Bearer ${token}`
         },
       });
       const result = await response.json();
@@ -79,14 +95,15 @@ const AnnonceList = () => {
       setLoading(false);
     }
   };
-
   const handleAnnonceAll = async () => {
     try {
       setLoading(true);
       await new Promise(resolve => setTimeout(resolve, 1000));
+
+  
       const response = await fetch('https://okazcar.up.railway.app/voitureUtilisateurs_validated', {
         headers: {
-          'Authorization': token()
+          Authorization: `Bearer ${token}`
         },
       });
       const result = await response.json();
@@ -100,7 +117,7 @@ const AnnonceList = () => {
   };
 
   if (loading) {
-    return (<Col className="text-center">loading...</Col>);
+    return(<Col className="text-center"><RingLoader style={{margin:"0px auto 0 auto"}} color={'purple'} loading={loading} size={60} /></Col> );
   }
 
   if (error) {
@@ -111,12 +128,12 @@ const AnnonceList = () => {
     <div className="header bg-gradient-purple pb-8 pt-5 pt-md-8">
       <Container fluid>
         <div className="header-body">
-          <Row style={{ margin: '0 0px 3% 90%' }}>
-            <div
+          <Row style={{ margin: '-7% 0px 3% 90%' }}>
+          <div
               className="icon icon-shape bg-success text-white rounded-circle shadow"
               id="tooltip-icon-tout"
               onClick={handleAnnonceAll}
-              style={{ cursor: "pointer" }}
+              style={{cursor:"pointer"}}
             >
               <i className="ni ni-align-center" />
               <UncontrolledTooltip placement="left" target="tooltip-icon-tout">
@@ -127,7 +144,7 @@ const AnnonceList = () => {
               className="icon icon-shape bg-danger text-white rounded-circle shadow"
               id="tooltip-icon"
               onClick={handleAnnonceClick}
-              style={{ margin: "0px 0px 0px 10px", cursor: "pointer" }}
+              style={{margin:"0px 0px 0px 10px",cursor:"pointer"}}
             >
               <i className="ni ni-bell-55" />
               <UncontrolledTooltip placement="left" target="tooltip-icon">
@@ -136,58 +153,58 @@ const AnnonceList = () => {
             </div>
           </Row>
           {data.map((annonce) => (
-            <Row key={annonce.id}>
-              <Col lg="2" xl="4">
-                <Card style={{ height: '60vh' }} className="card-stats mb-4 mb-xl-0">
-                  <CardBody>
-                    <Row>
-                      <Col>
-                        <CardTitle tag="h5" className="text-uppercase text-muted mb-0">
-                          {annonce.voitureUtilisateur?.utilisateur.username}
-                        </CardTitle>
-                        <span className="h2 font-weight-bold mb-0">
-                          {annonce.voitureUtilisateur?.voiture?.nom || annonce.voiture.nom} -{' '}
-                          {annonce.voitureUtilisateur?.voiture?.modele?.marque?.nom ||
-                            annonce.voiture.modele.marque.nom}{' '}
-                          {annonce.voitureUtilisateur?.voiture?.categorie?.nom ||
-                            annonce.voiture.categorie.nom} <br />
-                          Type: {annonce.voitureUtilisateur?.voiture?.type?.nom || annonce.voiture.type.nom}
-                        </span>
-                        <h5>Immatriculation: {annonce.voitureUtilisateur?.immatriculation || ''}</h5>
-                        <h4>
-                          Prix de vente: {annonce.prix || ''} {annonce.devise?.nom || ''} - Prix du commission:{' '}
-                          {annonce.prixCommission || ''} {annonce.devise?.nom || ''}
-                        </h4>
-
-                        <h4>Localisation: {annonce.voitureUtilisateur?.voiture?.localisation || ''}</h4>
-                        <h4>Date de demande: {annonce.voitureUtilisateur?.voiture?.dateDemande || ''}</h4>
-                      </Col>
-                      <img
+              <Row key={annonce.id}>
+                <Col lg="2" xl="4">
+                  <Card style={{ height: '60vh' }} className="card-stats mb-4 mb-xl-0">
+                    <CardBody>
+                      <Row>
+                        <Col>
+                          <CardTitle tag="h5" className="text-uppercase text-muted mb-0">
+                            {annonce.voitureUtilisateur?.utilisateur.username}
+                          </CardTitle>
+                          <span className="h2 font-weight-bold mb-0">
+                            {annonce.voitureUtilisateur?.voiture?.nom || annonce.voiture.nom} -{' '}
+                            {annonce.voitureUtilisateur?.voiture?.modele?.marque?.nom ||
+                              annonce.voiture.modele.marque.nom}{' '}
+                            {annonce.voitureUtilisateur?.voiture?.categorie?.nom ||
+                              annonce.voiture.categorie.nom} <br />
+                            Type: {annonce.voitureUtilisateur?.voiture?.type?.nom || annonce.voiture.type.nom}
+                          </span>
+                          <h5>Immatriculation: {annonce.voitureUtilisateur?.immatriculation || ''}</h5>
+                          <h4>
+                            Prix de vente: {annonce.prix || ''} {annonce.devise?.nom || ''} - Prix du commission:{' '}
+                            {annonce.prixCommission || ''} {annonce.devise?.nom || ''}
+                          </h4>
+                          <h4>Localisation: {annonce.voitureUtilisateur?.voiture?.localisation || ''}</h4>
+                          <h4>Date de demande: {annonce.voitureUtilisateur?.voiture?.dateDemande || ''}</h4>
+                        </Col>
+                        <img
                           alt="(impossible de faire un rendu de l'image "
                           src={`data:image/png;base64,${annonce.voitureImage[0]}`}
                       />
-                      {!isListValidated ? (
-                        <>
-                          <Button
-                            style={{ margin: "20px 0px 0px 0px", backgroundColor: "lightgreen", color: "white" }}
-                            onClick={() => handleValidate(annonce.id)}
-                          >
-                            Valider
-                          </Button>
-                          <Button
-                            style={{ margin: "20px 0px 0px 5%", backgroundColor: "lightsalmon", color: "white" }}
-                            onClick={() => handleReject(annonce.id)}
-                          >
-                            Refuser
-                          </Button>
-                        </>
-                      ) : ""}
-                    </Row>
-                  </CardBody>
-                </Card>
-              </Col>
-            </Row>
-          ))}
+                       {!isListValidated ? (
+                          <>
+                            <Button
+                              style={{ margin: "20px 0px 0px 0px", backgroundColor: "lightgreen", color: "white" }}
+                              onClick={() => handleValidate(annonce.id)}
+                            >
+                              Valider
+                            </Button>
+                            <Button 
+                              style={{ margin: "20px 0px 0px 5%", backgroundColor: "lightsalmon", color: "white" }}
+                              onClick={() => handleReject(annonce.id)}
+                            >
+                              Refuser
+                            </Button>
+                          </>
+                        ) : ""}
+                      </Row>
+                    </CardBody>
+                  </Card>
+                </Col>
+              </Row>
+            ))}
+
         </div>
       </Container>
     </div>
@@ -195,3 +212,5 @@ const AnnonceList = () => {
 };
 
 export default AnnonceList;
+
+
