fbc50079734c89f0da0832ac3ad65d5648053171
Rakotomavo Manoa Harivony Ellias <114306980+HarivonyEllias@users.noreply.github.com>
2024-01-29 14:21:17 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,12 +8,34 @@ import "assets/scss/argon-dashboard-react.scss";
 
 import AdminLayout from "layouts/Admin.js";
 import AuthLayout from "layouts/Auth.js";
-import Index from "views/Index.js";
+import Sidebar from "components/Sidebar/Sidebar";
+import Annonce from "views/annnonce/Annonce";
+import Index from "views/Index";
+import routes from "routes";
+import Icons from "views/examples/Icons";
+
 function App(){
     return(
         <>
-            <Routes>
-            <Route path="/test" element={<Index />} />
+           <Routes>
+           <Route path="/admin/annonces" element={
+                <>
+                    <Sidebar 
+                    routes={routes}
+                    />
+                    <Icons />
+                </>
+            } 
+            />
+            <Route path="/admin/statistiques" element={
+                <>
+                    <Sidebar 
+                    routes={routes}
+                    />
+                    <Index />
+                </>
+            } 
+            />
             <Route path="/auth/*" element={<AuthLayout />} />
             <Route path="*" element={<Navigate to="/admin/index" replace />} />
             </Routes>
